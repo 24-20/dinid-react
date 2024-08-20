724d7673c3f4939532c6f732b8632f9a91210d7e
24-20 <106987713+24-20@users.noreply.github.com>
2024-08-20 22:15:51 +0200

    updating db

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -2,9 +2,9 @@
 
 const Contact = () => {
   return (
-    <div className=' w-screen h-[calc(100vh-74px)] flex flex-col items-center justify-center gap-3'>
-            <h1 className=' text-lg'>Send en Email til adressen under for mer detaljer</h1>
-            <h1 className='text-gray-500 mt-4 underline'>jonasskog@gmail.com</h1>
+    <div className=' w-screen h-[calc(100vh-74px)] flex flex-col items-center justify-center gap-3 px-8'>
+            <h1 className=' text-lg text-center'>Send en Email til adressen under for mer detaljer</h1>
+            <h1 className='text-gray-500 mt-4 underline'>jonasskogjobb@gmail.com</h1>
 
 
         </div>
